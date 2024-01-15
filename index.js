51a0f874bb095d710da611a087f5e1c228f7ea29
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-01-15 19:44:40 +1300

    updates

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const users = {};
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+    socket.on("connected", (name) => {
+        eval("users." + name + " = " + socket.id);
+    });
+    socket.on("refreshNames", () => {
+        io.emit(users)
+    })
+});
